e4b11708aba8040af2a1612c17808a7581fe8cd0
黃佳晨 <battle0450@Battle0450.local>
2020-12-14 21:41:15 +0800

    Parallel PSO.version_2

--- PPSO.py
+++ PPSO.py
@@ -4,7 +4,7 @@ import random
 import time                                   
 
 n_iterations = 100
-n = 50000
+n = 10000
 c1 = 0.1
 c2 = 0.1
 W = 0.2
@@ -43,57 +43,13 @@ class Space():
                 particle.pbest_value = fitness
                 particle.pbest_position = particle.position
 
-        # MPI Part
-        # if(world_rank == 0): 
-        #     i = 0
-        #     avgworkload = int(n / world_size)
-        #     for particle in self.particles[i:i+avgworkload]:
-        #         fitness = self.fitness(particle)
-        #         if(fitness < particle.pbest_value):
-        #             particle.pbest_value = fitness
-        #             particle.pbest_position = particle.position
-        #     i = avgworkload
-        #     for des in range(2, world_size):
-        #         comm.send(self.particles[i:i+avgworkload], dest = des, tag = 1)
-        #         i+=avgworkload
-        # else:
-        #     print(world_rank)
-        #     data = comm.recv(source = 0, tag = 1)
-        #     for particle in data:
-        #         fitness = self.fitness(particle)
-        #         if(fitness < particle.pbest_value):
-        #             particle.pbest_value = fitness
-        #             particle.pbest_position = particle.position
-
     def update_gbest(self):
         for particle in self.particles:
             fitness = self.fitness(particle)
             if(fitness < self.gbest_value):
                 self.gbest_value = fitness
                 self.gbest_position = particle.position
-                #print(self.gbest_value)
-
-        # MPI part
-        # if(world_rank == 0): 
-        #     i = 0
-        #     avgworkload = int(n_particles / world_size)
-        #     for particle in self.particles[i:i+avgworkload]:
-        #         fitness = self.fitness(particle)
-        #         if(fitness < particle.pbest_value):
-        #             particle.pbest_value = fitness
-        #             particle.pbest_position = particle.position
-        #     for des in range(2, world_size, avgworkload):
-        #         comm.send(self.particles[des:des+avgworkload], dest = des, tag = 1)
-        #         i+=avgworkload
-        #     for source in range(1, world_size):
-                
-        # else:
-        #     data = comm.recv(source = 0, tag = 1)
-        #     for particle in data:
-        #         fitness = self.fitness(particle)
-        #         if(fitness < particle.pbest_value):
-        #             particle.pbest_value = fitness
-        #             particle.pbest_position = particle.position        
+                #print(self.gbest_value)    
 
     def move_particles(self):
         for particle in self.particles:
@@ -107,31 +63,25 @@ class Space():
 if __name__ == '__main__':
     iteration = 0
     global_space = Space(n)
-    avg = (int(n / world_size))
+    avg = int(n / world_size)
     space = Space(avg)
     particles_swarm = [Particle() for _ in range(avg)]
     space.particles = particles_swarm
-    start = time.time()
+    start = MPI.Wtime()
 
+    while(iteration < n_iterations):
+        space.update_pbest()
+        iteration+=1
     if(world_rank != 0):
-        while(iteration < n_iterations):
-            space.update_pbest()
-            iteration+=1
         comm.send(space.particles, dest = 0, tag = 0)
-    else:
-        while(iteration < n_iterations):
-            space.update_pbest()
-            iteration+=1
-        iteration = 0
+    iteration = 0
 
     if(world_rank == 0):
         for src in range(1, world_size):
             local = comm.recv(source = src, tag = 0)
-            space.particles = space.particles + local
+            global_space.particles = global_space.particles + local
         while(iteration < n_iterations):
             space.update_gbest()
             space.move_particles()
             iteration +=1
-        print("Executing time: ", time.time() - start)
-    
-    
+        print("Executing time: ", MPI.Wtime() - start)
